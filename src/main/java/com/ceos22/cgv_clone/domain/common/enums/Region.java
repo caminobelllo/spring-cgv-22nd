58fb6cbf6f69190a61872b47a71d861cc20ae8a8
caminobelllo <alstpgml0812@gmail.com>
2025-09-27 13:02:18 +0900

    Refactor: enum descriptions

--- src/main/java/com/ceos22/cgv_clone/domain/common/enums/Region.java
+++ src/main/java/com/ceos22/cgv_clone/domain/common/enums/Region.java
@@ -1,13 +1,20 @@
 package com.ceos22.cgv_clone.domain.common.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Region {
-    SEOUL,
-    GYEONGGI,
-    INCHEON,
-    GANGWON,
-    DAEJEON_CHUNGCHEOUG,
-    DAEGU,
-    BUSAN_ULSAN,
-    GYUNGSANG,
-    GWANGJU_JEOLLA_JEJU;
+    SEOUL("서울"),
+    GYEONGGI("경기"),
+    INCHEON("인천"),
+    GANGWON("강원"),
+    DAEJEON_CHUNGCHEOUG("대전/충청"),
+    DAEGU("대구"),
+    BUSAN_ULSAN("부산/울산"),
+    GYUNGSANG("경상"),
+    GWANGJU_JEOLLA_JEJU("광주/전라/제주");
+
+    private final String regionName;
 }
--- src/main/java/com/ceos22/cgv_clone/domain/common/enums/SeatType.java
+++ src/main/java/com/ceos22/cgv_clone/domain/common/enums/SeatType.java
@@ -1,5 +1,13 @@
 package com.ceos22.cgv_clone.domain.common.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum SeatType {
-    STANDARD, DISABLED
+    STANDARD("일반석"),
+    DISABLED("장애인석");
+
+    private final String type;
 }
