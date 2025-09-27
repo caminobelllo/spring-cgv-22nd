53e8190181b49164233eb26c67a7e9ae6b7765ca
caminobelllo <alstpgml0812@gmail.com>
2025-09-27 12:44:30 +0900

    Fix: 영화관-상영관 조회 jpql 쿼리로 변경

--- src/main/java/com/ceos22/cgv_clone/domain/theater/repository/CinemaRepository.java
+++ src/main/java/com/ceos22/cgv_clone/domain/theater/repository/CinemaRepository.java
@@ -1,4 +1,23 @@
 package com.ceos22.cgv_clone.domain.theater.repository;
 
-public interface CinemaRepository {
+import com.ceos22.cgv_clone.domain.common.enums.Region;
+import com.ceos22.cgv_clone.domain.theater.entity.Cinema;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CinemaRepository extends JpaRepository<Cinema, Long> {
+
+    List<Cinema> findByRegion(Region region);
+
+    @Query("""
+        SELECT c FROM Cinema c
+        JOIN FETCH c.auditoriums
+        WHERE c.id = :id
+    """)
+    Optional<Cinema> findByIdWithAuditoriums(@Param("id") Long cinemaId);
+
 }
