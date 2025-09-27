c91ee4db44f20765478218cd5800bea0c98fd332
caminobelllo <alstpgml0812@gmail.com>
2025-09-27 14:36:23 +0900

    Chore: swagger 설정

---
+++ src/main/java/com/ceos22/cgv_clone/global/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.ceos22.cgv_clone.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI swagger() {
+
+        Info info = new io.swagger.v3.oas.models.info.Info()
+                .title("CGV clone API")
+                .description("CEOS BE 22nd").version("0.0.1");
+
+        String securityScheme = "JWT TOKEN";
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securityScheme);
+
+        Components components = new Components()
+                .addSecuritySchemes(securityScheme, new SecurityScheme()
+                        .name(securityScheme)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("Bearer")
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .info(info)
+                .addServersItem(new Server().url("/"))
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
