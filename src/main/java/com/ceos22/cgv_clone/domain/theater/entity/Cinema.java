1f897063dca317899520d2567ea392a9629b9900
caminobelllo <alstpgml0812@gmail.com>
2025-09-27 12:42:28 +0900

    Fix: 연관관계 오류 수정

--- src/main/java/com/ceos22/cgv_clone/domain/theater/entity/Cinema.java
+++ src/main/java/com/ceos22/cgv_clone/domain/theater/entity/Cinema.java
@@ -1,7 +1,11 @@
 package com.ceos22.cgv_clone.domain.theater.entity;
 
+import com.ceos22.cgv_clone.domain.common.enums.Region;
 import jakarta.persistence.*;
 import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -12,9 +16,13 @@ public class Cinema {
     @Column(name = "cinema_id")
     private Long id;
 
-    @Column(nullable = false, length = 50)
-    private String region;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Region region;
 
     @Column(nullable = false, length = 50)
     private String name;
+
+    @OneToMany(mappedBy = "cinema")
+    private List<Auditorium> auditoriums = new ArrayList<>();
 }
