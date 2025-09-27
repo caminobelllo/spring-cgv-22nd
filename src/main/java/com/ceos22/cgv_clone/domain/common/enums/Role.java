d8f0a7c028690bfebec3d1c82ad6b7ccdd61b607
caminobelllo <alstpgml0812@gmail.com>
2025-09-27 18:15:43 +0900

    Chore: role 추가

---
+++ src/main/java/com/ceos22/cgv_clone/domain/common/enums/Role.java
@@ -0,0 +1,14 @@
+package com.ceos22.cgv_clone.domain.common.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+
+    USER("ROLE_USER"),
+    ADMIN("ROLE_ADMIN");
+
+    private final String key;
+}
--- src/main/java/com/ceos22/cgv_clone/domain/member/entity/Member.java
+++ src/main/java/com/ceos22/cgv_clone/domain/member/entity/Member.java
@@ -1,5 +1,6 @@
 package com.ceos22.cgv_clone.domain.member.entity;
 
+import com.ceos22.cgv_clone.domain.common.enums.Role;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -23,11 +24,16 @@ public class Member {
     @Column(nullable = false, length = 20)
     private String nickname;
 
+    @Enumerated(EnumType.STRING) // Enum 타입을 문자열로 DB에 저장
+    @Column(nullable = false)
+    private Role role;
+
     public static Member create(String email, String password, String nickname) {
         Member member = new Member();
         member.email = email;
         member.password = password;
         member.nickname = nickname;
+        member.role = Role.USER;    // 기본은 USER로 생성됨
 
         return member;
     }
