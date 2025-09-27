d98aa598a3020c763c14616d98873d0ab625dcd0
caminobelllo <alstpgml0812@gmail.com>
2025-09-27 13:47:29 +0900

    Fix:조회 쿼리 적용

--- src/main/java/com/ceos22/cgv_clone/domain/booking/repository/BookingSeatRepository.java
+++ src/main/java/com/ceos22/cgv_clone/domain/booking/repository/BookingSeatRepository.java
@@ -1,15 +1,25 @@
 package com.ceos22.cgv_clone.domain.booking.repository;
 
+import com.ceos22.cgv_clone.domain.booking.eneity.Booking;
 import com.ceos22.cgv_clone.domain.booking.eneity.BookingSeat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface BookingSeatRepository extends JpaRepository<BookingSeat, Long> {
 
-    // 해당 상영의 이미 예매된 좌석
-    List<BookingSeat> findByScreeningId(Long screeningId);
+    @Query("""
+        SELECT bs FROM BookingSeat bs 
+        JOIN FETCH bs.seat 
+        WHERE bs.booking = :booking
+    """)
+    List<BookingSeat> findByBookingWithSeat(@Param("booking") Booking booking);
 
-    // 남은 좌석수
-    long countByScreeningId(Long screeningId);
+
+    void deleteByBooking(Booking booking);
+
+    // 좌석 현황 조회용
+    List<BookingSeat> findByScreeningId(Long screeningId);
 }
