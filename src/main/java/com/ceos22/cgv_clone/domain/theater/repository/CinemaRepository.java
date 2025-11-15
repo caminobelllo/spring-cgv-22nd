1da73450895198a44d6b363aa6c75d608b4265f2
caminobelllo <alstpgml0812@gmail.com>
2025-11-15 23:26:05 +0900

    Refactor: Cinema domain N+1 - @EntityGraph 설정

--- src/main/java/com/ceos22/cgv_clone/domain/theater/repository/CinemaRepository.java
+++ src/main/java/com/ceos22/cgv_clone/domain/theater/repository/CinemaRepository.java
@@ -2,8 +2,8 @@ package com.ceos22.cgv_clone.domain.theater.repository;
 
 import com.ceos22.cgv_clone.domain.common.enums.Region;
 import com.ceos22.cgv_clone.domain.theater.entity.Cinema;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
@@ -13,11 +13,7 @@ public interface CinemaRepository extends JpaRepository<Cinema, Long> {
 
     List<Cinema> findByRegion(Region region);
 
-    @Query("""
-        SELECT c FROM Cinema c
-        JOIN FETCH c.auditoriums
-        WHERE c.id = :id
-    """)
+    @EntityGraph(attributePaths = {"auditoriums", "auditoriums.auditoriumType"})
     Optional<Cinema> findByIdWithAuditoriums(@Param("id") Long cinemaId);
 
 }
